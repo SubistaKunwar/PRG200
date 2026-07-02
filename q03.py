0847cd3598408c40a65f2ad6f59cf04209f4cdc6
Subista Kunwar <subistakunwar@gmail.com>
2026-07-02 09:19:22 +0545

    do the questions that nirwan gave me

---
+++ q03.py
@@ -0,0 +1,7 @@
+# take an integer value input from a user and print "Jarona" if the number is 7, and "Wrong" if the number is anything else
+
+user_input = int(input("Enter an integer: "))
+if user_input == 7:
+    print("Jarona")
+else:
+    print("Wrong")
